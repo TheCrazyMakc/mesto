3bb2d65ea43822cef40f3f2388bb484aee2b1e62
@ochumelyi_maks <only-max@yandex.ru>
2022-02-20 11:29:21 +0300

    изменить мелкие ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const formElement = document.querySelector('.popup__form');
 const popupFieldName = popup.querySelector('.popup__field_name');
 const popupFieldDescription = popup.querySelector('.popup__field_description');
 const profileName = document.querySelector('.profile__name');
-const profileDescription = document.querySelector('.profile__text');
+const profileDescription = document.querySelector('.profile__description');
 
 const popupToggle = function() {
   popup.classList.toggle('popup_opened');
